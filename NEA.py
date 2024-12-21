528b444b5eb2def5fe5028d90955e89302ff8aad
Miles-1000 <72467602+Miles-1000@users.noreply.github.com>
2024-12-21 13:09:31 +0000

    Simple application using button and text

--- NEA.py
+++ NEA.py
@@ -1,1 +1,24 @@
+import sys
+from PySide6 import QtCore, QtWidgets, QtGui, QtGraphs;
 
+class MainGUI(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.button = QtWidgets.QPushButton("Buttony wuttony")
+        self.line_edit = QtWidgets.QLineEdit()
+        self.line_edit.setPlaceholderText("Button not pressed yet :(")
+        self.text_edit = QtWidgets.QTextEdit()
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.line_edit)
+        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.text_edit)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+    widget = MainGUI()
+    widget.resize(800, 600)
+    widget.show()
+    app.exec()
+    sys.exit(app.exec())
