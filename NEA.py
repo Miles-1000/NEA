9025080db12a36b04ef72b26cca66b5880c848bc
Miles-1000 <72467602+Miles-1000@users.noreply.github.com>
2024-12-22 19:08:13 +0000

    Algorithm selection screen with combo box

--- NEA.py
+++ NEA.py
@@ -2,34 +2,33 @@ import sys
 #from PySide6 import QtCore, QtWidgets, QtGui, QtGraphs
 from PySide6 import QtWidgets, QtCore
 
-class MainGUI(QtWidgets.QWidget):
-    def __init__(self):
+class Display(QtWidgets.QWidget):
+    def __init__(self, algos):
         super().__init__()
-        
-        self.button = QtWidgets.QPushButton("Buttony wuttony")
-        self.input_box = QtWidgets.QLineEdit()
-        self.input_box.setPlaceholderText("Button not pressed yet :(")
-        self.text_edit = QtWidgets.QTextEdit()
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.input_box)
-        self.layout.addWidget(self.button)
-        self.layout.addWidget(self.text_edit)
 
-        self.button.clicked.connect(self.inputEntered)
-        self.button.clicked.connect(self.buttonPressed)
-        self.input_box.returnPressed.connect(self.inputEntered)
+        self.algos = algos
+        
+        self.setWindowTitle("Stock Market Trading Algorithm")
+        self.algoSelection = QtWidgets.QComboBox()
+        self.algoSelection.addItems(algos)
+        self.choiceDisplay = QtWidgets.QLabel()
+        self.choiceDisplay.setText("No algorithm selected yet")
+        self.layout = QtWidgets.QVBoxLayout()
+
+        self.layout.addWidget(self.algoSelection)
+        self.layout.addWidget(self.choiceDisplay)
+        self.setLayout(self.layout)
+
+        self.algoSelection.currentIndexChanged.connect(self.changeAlgo)
 
     @QtCore.Slot()
-    def inputEntered(self):
-        self.text_edit.append(self.input_box.text())
-        self.input_box.setText("")
+    def changeAlgo(self, index):
+        self.choiceDisplay.setText("Current Algorithm: " + self.algos[index])
 
-    def buttonPressed(self):
-        self.input_box.setPlaceholderText("Button pressed!! YAY :DD")
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    widget = MainGUI()
+    widget = Display(["RSI", "Moving average", "Custom"])
     widget.resize(800, 600)
     widget.show()
     sys.exit(app.exec())
