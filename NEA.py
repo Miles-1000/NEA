189bbc2a8bcc3deadaa49d38ed3a3897dde26a69
Miles-1000 <72467602+Miles-1000@users.noreply.github.com>
2025-01-03 15:00:34 +0000

    Data candlestick chart integration

--- NEA.py
+++ NEA.py
@@ -17,33 +17,33 @@ class Display(QMainWindow):
     def setupMainGUI(self):
         self.setWindowTitle("Stock Market Trading Algorithm")
 
-        #Setting central widget for QMainWindow 
+        # Setting central widget for QMainWindow 
         centralWidget = QWidget()
         self.setCentralWidget(centralWidget)
         self.mainLayout = QVBoxLayout()
 
-        #Defining dropdown box
+        # Defining dropdown box
         self.algoSelection = QComboBox()
         self.algoSelection.addItems(["No algorithm", "RSI", "Moving average"])
 
         self.algoSelection.currentIndexChanged.connect(self.changeAlgo)
         
-        #Defining text for algorithm selection
+        # Defining text for algorithm selection
         algoSelectText = QLabel("Select algorithm: ")
 
-        #Set layout for top controls
+        # Set layout for top controls
         controlsLayout = QHBoxLayout()
         controlsLayout.addWidget(algoSelectText)
         controlsLayout.addWidget(self.algoSelection)
 
-        #Adding control layout to main
+        # Adding control layout to main
         self.mainLayout.addLayout(controlsLayout)
         centralWidget.setLayout(self.mainLayout)
     
     def setupCandlestickChart(self):
-        #Initialise chart
+        # Initialise chart
         self.chart = QChart()
-        self.chart.setTitle("Example Chart")
+        self.chart.setTitle("S&P 500 Chart")
 
         self.candlestickChart = QCandlestickSeries()
         self.candlestickChart.setName("Stock Candlesticks")
@@ -52,53 +52,63 @@ class Display(QMainWindow):
 
         self.chart.addSeries(self.candlestickChart)
 
-        #Configuring date ranges
-        self.startDate = int(datetime.strptime("2024-01-10 14:30:00", "%Y-%m-%d %H:%M:%S").timestamp())
-        self.endDate = int(datetime.strptime("2024-01-10 19:00:00", "%Y-%m-%d %H:%M:%S").timestamp())
+        # Configuring date ranges
+        self.startDate = datetime.strptime("2024-01-10 14:30:00", "%Y-%m-%d %H:%M:%S")
+        self.startDateTimestamp = int(self.startDate.timestamp())
+        self.endDate = datetime.strptime("2024-01-10 19:00:00", "%Y-%m-%d %H:%M:%S")
+        self.endDateTimestamp = int(self.endDate.timestamp())
 
-        #Configuring x axis
+        # Retrieve data using IG API        
+        priceData = self.loadIGData("IX.D.SPTRD.DAILY.IP", "30Min", self.startDate, self.endDate)
+
+        df_ask = priceData['prices']['ask']
+
+        # Iterate through database rows
+        for time, price in df_ask.iterrows():
+            candleStick = QCandlestickSet(
+                open = price["Open"],
+                high = price["High"],
+                low = price["Low"],
+                close = price["Close"],
+                timestamp = QDateTime.fromSecsSinceEpoch(int(datetime.strptime(str(time), "%Y-%m-%d %H:%M:%S").timestamp())).toMSecsSinceEpoch()
+                )
+            
+            self.candlestickChart.append(candleStick)
+
+
+        # Configuring x axis
         self.xAxis = QDateTimeAxis()
-        self.xAxis.setTitleText("Day")
-        self.xAxis.setFormat("dd-MM-yyyy")
-        self.xAxis.setRange(QDateTime.fromSecsSinceEpoch(self.startDate - 86400), QDateTime.fromSecsSinceEpoch(self.endDate))
-        self.xAxis.setTickCount((self.endDate - self.startDate) // 86400 + 1)
+        self.xAxis.setTitleText("Time")
+        self.xAxis.setFormat("HH:mm")
+        self.xAxis.setRange(QDateTime.fromSecsSinceEpoch(self.startDateTimestamp), QDateTime.fromSecsSinceEpoch(self.endDateTimestamp))
 
-        #Configuring y axis
+        # Configuring y axis
         self.yAxis = QValueAxis()
         self.yAxis.setTitleText("Price")
-        self.yAxis.setRange(135,150)
+        self.yAxis.setRange(4745,4785)
 
-        #Adding axes to chart
+        # Adding axes to chart
         self.chart.addAxis(self.xAxis, Qt.AlignBottom)
         self.chart.addAxis(self.yAxis, Qt.AlignLeft)
 
         self.candlestickChart.attachAxis(self.xAxis)
         self.candlestickChart.attachAxis(self.yAxis)
 
-        self.loadIGData("IX.D.SPTRD.DAILY.IP",
-                        "30Min",
-                        self.startDate,
-                        self.endDate
-                        )
-
-        #View chart
+        # View chart
         self.chartView = QChartView(self.chart)
         self.chartView.setRenderHint(QPainter.Antialiasing)
 
-        #Configure layout
+        # Configure layout
         self.mainLayout.addWidget(self.chartView)
 
     def loadIGData(self, epic, resolution, startDate, endDate):
+        # Configure service
         ig_service = IGService(config.username, config.password, config.api_key, config.acc_type)
 
         ig_service.create_session()
 
-        response = ig_service.fetch_historical_prices_by_epic_and_date_range(epic, resolution, datetime.fromtimestamp(startDate).strftime("%Y-%m-%d %H:%M:%S"), datetime.fromtimestamp(endDate).strftime("%Y-%m-%d %H:%M:%S"))
-        
-        print(response)
-        
-        df_ask = response['prices']['ask']
-        print(f"ask prices:\n{df_ask}")       
+        # Retrieve data
+        return ig_service.fetch_historical_prices_by_epic_and_date_range(epic, resolution, startDate.strftime("%Y-%m-%d %H:%M:%S"), endDate.strftime("%Y-%m-%d %H:%M:%S"))
 
 
     @Slot(int)
